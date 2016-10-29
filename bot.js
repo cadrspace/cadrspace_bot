7cff0305cd1db2eae8be9770278f3db4f9edc784
overmind <seogarry@gmail.com>
2016-10-29 22:40:34 +0300

    Add camera command to show status with camera screenshot from hackspace

--- bot.js
+++ bot.js
@@ -3,13 +3,14 @@ var cfg = require('./config.'+env);
 
 var TelegramBot = require('node-telegram-bot-api');
 var Winston = require('winston');
+var fs = require('fs');
 var https = require('https');
 var token = cfg.telegramToken;
 var bot = new TelegramBot(token, { polling: true });
 
 var logger = new (Winston.Logger)({
     transports: [
-        new (Winston.transports.File)({ filename: 'bot.log' })
+	new (Winston.transports.File)({ filename: 'bot.log' })
     ]
 });
 
@@ -39,25 +40,44 @@ var logger = new (Winston.Logger)({
 
 function callSpaceApi(cb) {
     var options = {
-        host: 'cadrspace.ru',
-        path: '/status/json'
+	host: 'cadrspace.ru',
+	path: '/status/json'
     };
 
     var callback = function(response) {
-        var str = '';
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
+	var str = '';
+	response.on('data', function (chunk) {
+	    str += chunk;
+	});
 
-        response.on('end', function () {
-            var json = JSON.parse(str);
-            cb(json);
-        });
+	response.on('end', function () {
+	    var json = JSON.parse(str);
+	    cb(json);
+	});
     };
-    
+
     https.request(options, callback).end();
 }
 
+function getCameraShot(url, cb) {
+
+    var download = function(url, dest, callback) {
+	var file = fs.createWriteStream(dest);
+	https.get(url, function(response) {
+	    response.pipe(file);
+	    file.on('finish', function() {
+		file.close(callback);  // close() is async, call cb after close completes.
+	    });
+	}).on('error', function(err) { // Handle errors
+	    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+	    if (callback) callback(err.message);
+	});
+    };
+
+    //var url = 'http://nntc.nnov.ru:58080/?action=stream';
+    //var url = 'https://upload.wikimedia.org/wikipedia/commons/4/4a/1999_Subaru_Impreza_(MY99)_WRX_STI_Version_5_coupe_(23295614974).jpg'
+    download(url, 'image.jpg', cb);
+}
 
 bot.onText(/\/echo (.+)/, function (msg, match) {
     var fromId = msg.from.id;
@@ -74,13 +94,14 @@ bot.onText(/\/start/, function (msg) {
 bot.onText(/\/status/, function (msg) {
 
     var message;
+
     callSpaceApi(function(data) {
-        if (!data.state.open) {
-            message = '[ЗАКРЫТ] Сейчас хакспейс закрыт';
-        } else {
-            message = '[ОТКРЫТ] Сейчас хакспейс открыт';
-        }
-        bot.sendMessage(msg.from.id, message);
+	if (!data.state.open) {
+	    message = '[ЗАКРЫТ] Сейчас хакспейс закрыт';
+	} else {
+	    message = '[ОТКРЫТ] Сейчас хакспейс открыт';
+	}
+	bot.sendMessage(msg.from.id, message);
     });
 });
 
@@ -90,14 +111,35 @@ bot.onText(/\/help/, function (msg) {
     var message = '';
     bot.sendMessage(chatId, '/status - Посмотреть статус хакспейса' + '\n\n');
     callSpaceApi(function(data) {
-        message += data.space + '- это проект по созданию творческого пространства для реализации технических идей, исследований, обмена опытом, обучения и саморазвития, а также для простого человеческого общения на технические темы.\n\n'
-            + 'Address: ' + data.location.address + '\n\n'
-            + ''
-            + 'Twitter: ' + data.contact.twitter + '\n'
-            + 'IRC: ' + data.contact.irc + '\n'
-            + 'ML: ' + data.contact.ml + '\n';
-        
-        bot.sendMessage(msg.from.id, message);
+	message += data.space + '- это проект по созданию творческого пространства для реализации технических идей, исследований, обмена опытом, обучения и саморазвития, а также для простого человеческого общения на технические темы.\n\n'
+	    + 'Address: ' + data.location.address + '\n\n'
+	    + ''
+	    + 'Twitter: ' + data.contact.twitter + '\n'
+	    + 'IRC: ' + data.contact.irc + '\n'
+	    + 'ML: ' + data.contact.ml + '\n';
+
+	bot.sendMessage(msg.from.id, message);
+    });
+});
+
+
+bot.onText(/\/camera/, function (msg) {
+    var chatId = msg.chat.id;
+    var message;
+
+    callSpaceApi(function(data) {
+	if (!data.state.open) {
+	    message = '[ЗАКРЫТ] Сейчас хакспейс закрыт, изображение с камеры недоступно';
+	    var kittenUrl = 'https://thecatapi.com/api/images/get?format=src&type=jpg';
+	    getCameraShot(kittenUrl, function() {
+		bot.sendPhoto(chatId, 'image.jpg', {caption: 'Изображение с камеры недоступно'});
+	    });
+	} else {
+	    message = '[ОТКРЫТ] Сейчас хакспейс открыт';
+	    getCameraShot(data.cam[0], function() {
+		bot.sendPhoto(chatId, 'image.jpg', {caption: message});
+	    });
+	}
     });
 });
 
@@ -105,5 +147,3 @@ bot.onText(/\/help/, function (msg) {
 bot.on('message', function (msg) {
     logger.log('info', 'Message %s', msg.text);
 });
-
-
